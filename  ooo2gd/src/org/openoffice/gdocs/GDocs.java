4b8f4bfc54047cc34e91783d1a06b416adb998f9
przemelek <przemelek@ed438a90-bd37-0410-8c53-c376e5b3842d>
2007-08-15 14:28:55 +0000

    initial commit

---
+++  ooo2gd/src/org/openoffice/gdocs/GDocs.java
@@ -0,0 +1,150 @@
+package org.openoffice.gdocs;
+
+import com.sun.star.beans.PropertyValue;
+import com.sun.star.comp.helper.Bootstrap;
+import com.sun.star.frame.XDesktop;
+import com.sun.star.frame.XModel;
+import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.text.XTextDocument;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XComponentContext;
+import com.sun.star.lib.uno.helper.Factory;
+import com.sun.star.lang.XSingleComponentFactory;
+import com.sun.star.registry.XRegistryKey;
+import com.sun.star.lib.uno.helper.WeakBase;
+import com.sun.star.xml.dom.XDocument;
+import javax.swing.JOptionPane;
+
+
+public final class GDocs extends WeakBase
+   implements com.sun.star.lang.XServiceInfo,
+              com.sun.star.frame.XDispatchProvider,
+              com.sun.star.lang.XInitialization,
+              com.sun.star.frame.XDispatch
+{
+    private final XComponentContext m_xContext;
+    private com.sun.star.frame.XFrame m_xFrame;
+    private static final String m_implementationName = GDocs.class.getName();
+    private static final String[] m_serviceNames = {
+        "com.sun.star.frame.ProtocolHandler" };
+
+
+    public GDocs( XComponentContext context )
+    {
+        m_xContext = context;
+    };
+
+    public static XSingleComponentFactory __getComponentFactory( String sImplementationName ) {
+        XSingleComponentFactory xFactory = null;
+
+        if ( sImplementationName.equals( m_implementationName ) )
+            xFactory = Factory.createComponentFactory(GDocs.class, m_serviceNames);
+        return xFactory;
+    }
+
+    public static boolean __writeRegistryServiceInfo( XRegistryKey xRegistryKey ) {
+        return Factory.writeRegistryServiceInfo(m_implementationName,
+                                                m_serviceNames,
+                                                xRegistryKey);
+    }
+
+    // com.sun.star.lang.XServiceInfo:
+    public String getImplementationName() {
+         return m_implementationName;
+    }
+
+    public boolean supportsService( String sService ) {
+        int len = m_serviceNames.length;
+
+        for( int i=0; i < len; i++) {
+            if (sService.equals(m_serviceNames[i]))
+                return true;
+        }
+        return false;
+    }
+
+    public String[] getSupportedServiceNames() {
+        return m_serviceNames;
+    }
+
+    // com.sun.star.frame.XDispatchProvider:
+    public com.sun.star.frame.XDispatch queryDispatch( com.sun.star.util.URL aURL,
+                                                       String sTargetFrameName,
+                                                       int iSearchFlags )
+    {
+        if ( aURL.Protocol.compareTo("org.openoffice.gdocs.gdocs:") == 0 )
+        {
+            if ( aURL.Path.compareTo("Export to Google Docs") == 0 )
+                return this;
+        }
+        return null;
+    }
+
+    // com.sun.star.frame.XDispatchProvider:
+    public com.sun.star.frame.XDispatch[] queryDispatches(
+         com.sun.star.frame.DispatchDescriptor[] seqDescriptors )
+    {
+        int nCount = seqDescriptors.length;
+        com.sun.star.frame.XDispatch[] seqDispatcher =
+            new com.sun.star.frame.XDispatch[seqDescriptors.length];
+
+        for( int i=0; i < nCount; ++i )
+        {
+            seqDispatcher[i] = queryDispatch(seqDescriptors[i].FeatureURL,
+                                             seqDescriptors[i].FrameName,
+                                             seqDescriptors[i].SearchFlags );
+        }
+        return seqDispatcher;
+    }
+
+    // com.sun.star.lang.XInitialization:
+    public void initialize( Object[] object )
+        throws com.sun.star.uno.Exception
+    {
+        if ( object.length > 0 )
+        {
+            m_xFrame = (com.sun.star.frame.XFrame)UnoRuntime.queryInterface(
+                com.sun.star.frame.XFrame.class, object[0]);
+        }
+    }
+
+    // com.sun.star.frame.XDispatch:
+     public void dispatch( com.sun.star.util.URL aURL,
+                           com.sun.star.beans.PropertyValue[] aArguments )
+    {
+         if ( aURL.Protocol.compareTo("org.openoffice.gdocs.gdocs:") == 0 )
+        {
+            if ( aURL.Path.compareTo("Export to Google Docs") == 0 )
+            {
+                try {                    
+//                    XTextDocument xDoc = (XTextDocument) UnoRuntime.queryInterface(
+//                    XTextDocument.class, m_xFrame.getController().getModel());
+                    XModel xDoc = (XModel) UnoRuntime.queryInterface(
+                    XModel.class, m_xFrame.getController().getModel());
+
+                    
+                    String path = xDoc.getURL();
+                    path=path.replaceAll("%20"," ");                    
+                    path=path.substring(8);                   
+                    //JOptionPane.showMessageDialog(null,path);
+                    new Thread(new Uploader(path)).start();
+                    //Uploader.performUpload(path);
+                } catch (Exception e) {};
+                return;
+            }
+        }
+    }
+
+    public void addStatusListener( com.sun.star.frame.XStatusListener xControl,
+                                    com.sun.star.util.URL aURL )
+    {
+        // add your own code here
+    }
+
+    public void removeStatusListener( com.sun.star.frame.XStatusListener xControl,
+                                       com.sun.star.util.URL aURL )
+    {
+        // add your own code here
+    }
+
+}
---
+++  ooo2gd/src/org/openoffice/gdocs/Uploader.java
@@ -0,0 +1,96 @@
+package org.openoffice.gdocs;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.JDialog;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import com.google.gdata.client.docs.DocsService;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.docs.DocumentEntry;
+import com.google.gdata.data.docs.DocumentListEntry;
+import com.google.gdata.data.docs.DocumentListFeed;
+import com.google.gdata.util.AuthenticationException;
+import org.openoffice.gdocs.ui.UploadDialog;
+import org.openoffice.gdocs.ui.Uploading;
+
+public class Uploader implements Runnable {	
+	public static final String APP_NAME = "RMK OpenOffice.org Docs Uploader";
+	public static final String DOCS_FEED = "http://docs.google.com/feeds/documents/private/full";
+        private String pathName;
+	private DocsService service;
+	
+        public Uploader(String pathName) {
+            this.pathName = pathName;
+        }
+        
+	public void login(String userName,String password) throws AuthenticationException {
+		service = new DocsService(APP_NAME);
+		service.setUserCredentials(userName,password);
+	}
+	
+	public boolean upload(String path) throws Exception {
+              boolean result = false; 
+              DocumentEntry newDocument = new DocumentEntry();
+              File documentFile = new File(path);
+              newDocument.setFile(documentFile);
+              newDocument.setTitle(new PlainTextConstruct(documentFile.getName()));
+              URL documentListFeedUrl = new URL(DOCS_FEED);
+              DocumentListEntry uploaded = service.insert(documentListFeedUrl, 
+                  newDocument);
+              result=true;
+              return result;
+	}
+	
+	public List<DocumentListEntry> getListOfDocs() {
+		List<DocumentListEntry> list = new LinkedList<DocumentListEntry>();
+		try {
+			URL documentFeedUrl = new URL(DOCS_FEED); 
+			DocumentListFeed feed = service.getFeed(documentFeedUrl,DocumentListFeed.class);
+			list=feed.getEntries();
+		} catch (Exception e) {
+				
+		}
+		return list;
+	}
+	
+	public List<String> list() {
+		List<String> list = new LinkedList<String>();
+		for (DocumentListEntry entry:getListOfDocs()) {
+			list.add(entry.getTitle().getPlainText());	
+		}
+		return list;
+	}
+	        
+        
+        public void run() {
+            UploadDialog form = new UploadDialog();
+            form.setMessageText("File "+pathName+" will be uploaded to Google Docs");
+            form.setModal(true);
+            form.toFront();
+            form.setVisible(true);
+            Uploading uploading = new Uploading();
+            if (form.getUpload()) {               
+                try {                    
+                    uploading.setVisible(true);
+                    login(form.getUserName(),form.getPassword());
+                    upload(pathName);
+                    JOptionPane.showMessageDialog(null,"File Uploaded");
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null,"Problem: "+e.getMessage());
+                }
+                finally {
+                    uploading.setVisible(false);
+                }
+                
+            }
+        
+        }
+        
+	
+}
