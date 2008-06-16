e2c8c400e350bbc6bd7376e0e63a196e467dc662
przemelek <przemelek@ed438a90-bd37-0410-8c53-c376e5b3842d>
2008-06-16 23:38:16 +0000

    Added Italian translation and new icons, version number increased to 1.1.2 [I still don't know if it will be release number ;-)]

---  ooo2gd/src/org/openoffice/gdocs/configuration/Configuration.java
+++  ooo2gd/src/org/openoffice/gdocs/configuration/Configuration.java
@@ -28,7 +28,7 @@ import org.openoffice.gdocs.util.Util;
 public class Configuration {
 
     private static final int MAX_SIZE_OF_LOG = 1000;
-    private static String versionStr = "1.1.1";
+    private static String versionStr = "1.1.2";
     private static List<String> log = new ArrayList<String>();
     private static boolean useProxy;
     private static boolean proxyAuth;
@@ -49,6 +49,7 @@ public class Configuration {
         map.put("Polski","pl");                
         map.put("Russian","ru");
         map.put("Spanish","es");
+        map.put("Italian","it");
         map.put("System","system");
         for (Entry<String, String> entry : map.entrySet()) {
             langsMap.put(entry.getValue(),entry.getKey());
